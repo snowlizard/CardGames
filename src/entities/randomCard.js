7a8913fb0fb15dcd44a9b49640affcc2b12e9f7a
snowlizard <illegal_burrito@live.com>
2021-11-10 17:18:52 -0800

    switching cards to group

---
+++ src/entities/randomCard.js
@@ -0,0 +1,18 @@
+import Phaser from "phaser";
+import { deck, randomCardIndex } from "../mixins/deck";
+
+
+export class RandomCard extends Phaser.GameObjects.Image{
+    constructor(scene, x, y){
+        super(scene, x, y);
+
+        this.scene = scene;
+        this.setX = x;
+        this.setY = y;
+        this.setScale(0.8);
+        this.scene.add.existing(this);
+        this.index = randomCardIndex();
+        this.setTexture(deck[this.index][0]);
+        this.setData('value', deck[this.index][1]);
+    };
+}
--- src/scenes/solitaire.js
+++ src/scenes/solitaire.js
@@ -1,4 +1,5 @@
 import { Scene } from "phaser";
+import { RandomCard } from "../entities/randomCard";
 import { Button } from '../mixins/button';
 import { deck, randomCardIndex } from '../mixins/deck';
 
@@ -8,55 +9,50 @@ class Solitaire extends Scene{
      */
     constructor(){
         super('solitaire');
-        this.playerCards = { 'card1': [300, 400], 'card2': [390, 400]};
-        this.dealerCards = { 'card1': [300, 100], 'card2': [390, 100]};
+        this.playerCardsXY = [300, 400, 390, 400];
+        this.dealerCardsXY = [300, 100, 390, 100];
         this.newCardXcoords = [390];
         this.newDealerXcoord= [390];
         this.temporaryCards = [];
         this.playerTotal = 0;
         this.dealerTotal = 0;
         this.round_over  = false;
+
     };
 
     create = () => {
+        // Card groups
+        this.cards = this.add.group({classType: RandomCard})
+        this.playerCardList = this.cards.getChildren()
+
+        this.dealerCards = this.add.group({classType: RandomCard});
+        this.dealerCardList = this.dealerCards.getChildren()
+
         // Player cards 
-        this.playerIndex1 = randomCardIndex();
-        this.playerIndex2 = randomCardIndex();
-        this.playerCard1 = this.add.image(this.playerCards.card1[0], this.playerCards.card1[1], deck[this.playerIndex1][0]).setScale(0.8);
-        this.playerCard2 = this.add.image(this.playerCards.card2[0], this.playerCards.card2[1], deck[this.playerIndex2][0]).setScale(0.8);
-        this.playerTotal = deck[this.playerIndex1][1] + deck[this.playerIndex2][1];
+        this.cards.add(new RandomCard(this, this.playerCardsXY[0], this.playerCardsXY[1]));
+        this.cards.add(new RandomCard(this, this.playerCardsXY[2], this.playerCardsXY[3]));
+        this.playerTotal = this.playerCardList[0].getData('value') + this.playerCardList[1].getData('value');
 
         // Dealer cards
-        this.dealerIndex1 = randomCardIndex();
-        this.dealerIndex2 = randomCardIndex();
-        this.dealerCard1 = this.add.image(this.dealerCards.card1[0], this.dealerCards.card1[1],deck[this.dealerIndex1][0]).setScale(0.8);
-        this.dealerCard2 = this.add.image(this.dealerCards.card2[0], this.dealerCards.card2[1],deck[this.dealerIndex2][0]).setScale(0.8);
-        this.dealerTotal = deck[this.dealerIndex1][1] + deck[this.dealerIndex2][1];
+        this.dealerCards.add(new RandomCard(this, this.dealerCardsXY[0], this.dealerCardsXY[1]));
+        this.dealerCards.add(new RandomCard(this, this.dealerCardsXY[2], this.dealerCardsXY[3]));
+        this.dealerTotal = this.dealerCardList[0].getData('value') + this.dealerCardList[1].getData('value');
 
         // give player another card
+        // need to add logic for ace
         this.Hit = new Button(this, 100, 350, 'Hit', () => {
             let tmpIndex = randomCardIndex();
             let tmpCard = this.add.image(this.newCardXcoords[this.newCardXcoords.length -1] + 20, 400, deck[tmpIndex][0]).setScale(0.8);
             this.newCardXcoords.push(this.newCardXcoords[this.newCardXcoords.length - 1] + 20);
             this.playerTotal += deck[tmpIndex][1];
             this.temporaryCards.push(tmpCard);
+
+            if(this.playerTotal > 21) this.round_over = true
         });
 
         // Dealer takes turn
         this.Stand = new Button(this, 100, 410, 'Stand', () => {
-            if (this.dealerTotal >= 17 ){
-                this.round_over = true;
-            }else{
-                while (this.dealerTotal < 17){
-                    let tempIndex = randomCardIndex()
-                    let tempCard  = this.add.image(this.newDealerXcoord[this.newDealerXcoord.length -1] + 20, 100, deck[tempIndex][0]).setScale(0.8);
-                    this.newDealerXcoord.push(this.newDealerXcoord[this.newDealerXcoord.length - 1] + 20);
-                    this.temporaryCards.push(tempCard)
-                    this.dealerTotal += deck[tempIndex][1]
-                };
-                this.round_over = true;
-            }
-
+            this.round_over = true;
         });
 
         console.log(this.playerTotal)
@@ -69,6 +65,15 @@ class Solitaire extends Scene{
             this.Hit.tint = 0.5;
         }
         if(this.round_over){
+            // Dealer takes turn
+            while (this.dealerTotal < 17){
+                let tempIndex = randomCardIndex()
+                let tempCard  = this.add.image(this.newDealerXcoord[this.newDealerXcoord.length -1] + 20, 100, deck[tempIndex][0]).setScale(0.8);
+                this.newDealerXcoord.push(this.newDealerXcoord[this.newDealerXcoord.length - 1] + 20);
+                this.temporaryCards.push(tempCard)
+                this.dealerTotal += deck[tempIndex][1]
+            };
+            
             this.Hit.setInteractive();
             if(this.playerTotal == 21) console.log("Blackjack")
             else if (this.playerTotal > 21 && this.dealerTotal > 21 ) console.log("It's a draw")
@@ -76,6 +81,9 @@ class Solitaire extends Scene{
             else if (this.dealerTotal > 21) console.log("You win")
             else if (this.playerTotal > this.dealerTotal) console.log("You win")
             else console.log("You lose")
+        }
+
+        if(this.reset){
             this.temporaryCards.forEach( card => {
                 card.destroy()
             });
@@ -109,11 +117,3 @@ class Solitaire extends Scene{
 };
 
 export default Solitaire;
-
-
-/*
-this.playerIndex1 = randomCardIndex();
-this.playerIndex2 = randomCardIndex();
-this.playerCard1.setTexture(deck[this.playerIndex1][0]);
-this.playerCard2.setTexture(deck[this.playerIndex2][0]);
-*/
